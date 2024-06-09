ef15c1f2488b1b5ea3f09e847ea579f46b6faad1
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2024-06-09 18:06:38 +0200

    add cover image and correct blog content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,22 @@
 import { z, defineCollection } from 'astro:content'
 
 const blogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        excerpt: z.string(),
-        href: z.string(),
-        // description: z.string(),
-        publishedDate: z.string(),
-        category: z.string(),
-        // image: z.object({
-        //     path: z.string(),
-        //     alt: z.string()
-        // })
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    image: z.string(),
+    excerpt: z.string(),
+    href: z.string(),
+    // description: z.string(),
+    publishedDate: z.string(),
+    category: z.string(),
+    // image: z.object({
+    //     path: z.string(),
+    //     alt: z.string()
+    // })
+  }),
 })
 
 export const collections = {
-    blog: blogCollection
+  blog: blogCollection,
 }
