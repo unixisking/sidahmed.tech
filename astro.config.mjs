3f1c6706cd3f2d7e44a5f2ff96c891887fcc06f8
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2024-06-09 05:43:37 +0200

    feat: add new blog post: setup typescript in new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
+
+import { remarkReadingTime } from './src/utils/remark-reading-time.mts'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    syntaxHighlight: "prism"
+    syntaxHighlight: 'prism',
+    remarkPlugins: [remarkReadingTime],
   },
-  integrations: [tailwind(), mdx(), preact()]
-});
+  integrations: [tailwind(), mdx(), preact()],
+})
